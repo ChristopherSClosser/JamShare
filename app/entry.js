59524da353f6372ab402110651c8eb749add8c38
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-06-19 14:26:55 -0700

    entry.js

--- app/entry.js
+++ app/entry.js
@@ -1,21 +1,21 @@
-'use strict';
+'use strict'
 
-const path = require('path');
-const camelcase = require('camelcase');
-const pascalcase = require('pascalcase');
-const angular = require('angular');
-require('@uirouter/angularjs');
+const path = require('path')
+const camelcase = require('camelcase')
+const pascalcase = require('pascalcase')
+const angular = require('angular')
+require('@uirouter/angularjs')
 
-const cfgram = angular.module('jamShare', ['ui.router']);
+const jamShare = angular.module('jamShare', ['ui.router'])
 
-let context = require.context('./config/', true, /\.js$/);
-context.keys().forEach(key => jamShare.config(context(path)));
+let context = require.context('./config/', true, /\.js$/)
+context.keys().forEach(path => jamShare.config(context(path)))
 
-context = require.context('./view/', true, /\.js$/);
-context.keys().forEach(key => jamShare.controller(pascalcase(path.basename(key, '.js')), context(key)));
+context = require.context('./view/', true, /\.js$/)
+context.keys().forEach(key => jamShare.controller(pascalcase(path.basename(key, '.js')), context(key)))
 
-context = require.context('./service/', true, /\.js$/);
-context.keys().forEach(key => jamShare.service(camelcase(path.basename(key, '.js')), context(key)));
+context = require.context('./service/', true, /\.js$/)
+context.keys().forEach(key => jamShare.service(camelcase(path.basename(key, '.js')), context(key)))
 
-context = require.context('./component/', true, /\.js$/);
-context.keys().forEach(key => jamShare.component(camelcase(path.basename(key, '.js')), context(key)));
+context = require.context('./component/', true, /\.js$/)
+context.keys().forEach(key => jamShare.component(camelcase(path.basename(key, '.js')), context(key)))
